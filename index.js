b4f23f55c7eee437823d122c9afa16337b54e6de
AdrienMS <adrien.musserotte@gmail.com>
2019-06-13 13:08:32 +0200

    fix (Error) : Correct some bugs
    When a route was not found, the server redirect to the documentation page.
    Fix message how not appeared in errors.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const db = require('./config/database');
 const server = require('./config/express');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 if (!module.parent) {
     server.listen(PORT, () => console.info(`Server running on ${PORT}`));
